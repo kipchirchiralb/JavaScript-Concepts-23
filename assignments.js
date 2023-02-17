1d05b4887a3e5f8b71181532f6798adb8a4f732c
albertkipchirchir <albert@eldohub.co.ke>
2023-02-17 11:53:27 +0300

    more assignments done

--- assignments.js
+++ assignments.js
@@ -43,3 +43,42 @@ const person = {
 // console.log(person.name, person.age, person.city, person["went to"]);
 
 // Write a function that takes an array of numbers as an argument and returns the sum of the numbers in the array. Then, call the function and print the result to the console. //reduce() array method
+
+let numbersArray = [1, 2, 3, 4, 45, 554, 65, 65, 6, 44, 3];
+
+function sumNumbersInArray(array) {
+  let sum = 0;
+  for (let index = 0; index < array.length; index++) {
+    sum = sum + array[index];
+  }
+  // return array.reduce((prev, next) => (prev = prev + next));
+  return sum;
+}
+let result = sumNumbersInArray(numbersArray);
+// console.log(result);
+
+// higher - - - return func && take in a func as an argument
+
+// Palindrome checker: Write a function that takes a string as an argument and returns a boolean indicating whether the string is a palindrome (a word or phrase that reads the same forwards and backwards). You can use string methods such as split(), reverse(), and join() to accomplish this task.
+// madam , level
+function isStringPalindrome(string) {
+  // return either true or false
+  // split the string to get an array of the characters
+  // reverse the array
+  // join the reversed array to get a finalString
+  // compare the initial string to the final string
+  // let initialString = string;
+  // let finalString = "";
+  // let stringArray = initialString.split("");
+  // let reversedArray = stringArray.reverse();
+  // finalString = reversedArray.join("");
+  // return initialString === finalString;
+  // console.log(typeof string.split("").reverse().join(""));
+  return string === string.split("").reverse().join("");
+}
+
+// console.log(isStringPalindrome("helllo")); // complexity - time & space ) - Big O notation
+
+// console.log("car" === "rac");
+
+// Longest word in a string: Write a function that takes a string as an argument and returns the longest word in the string. You can use string methods such as split(), sort(), and length to accomplish this task.
